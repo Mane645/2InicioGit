af45ed107e970c569affa51a2efb171dfb285245
Jose Emmanuel Rangel Rodriguez <je.rangelrodriguez@ugto.mx>
2023-02-02 17:51:55 -0600

    arregle la animacion

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'Es Que Yo Te Quiero A Ti',
+        path: '../music/Song 1.mp3',
+        artist: 'Kevin Kaarl',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'Colapso',
+        path: '../music/Song 2.mp3',
+        artist: 'Kevin Kaarl',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'como me encanta',
+        path: '../music/Song 3.mp3',
+        artist: 'Kevin Kaarl',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Toda esta ciudad',
+        path: '../music/Song 4.mp3',
+        artist: 'Kevin Kaarl',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'te quiero tanto',
+        path: '../music/Song 5.mp3',
+        artist: 'Kevin Kaarl',
+        cover: '../images/cover 5.png'
+    }
+]
